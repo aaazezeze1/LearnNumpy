9302c016a6ef633549c4a92d0563478405ab66bb
Amazing Cabiles <amazinggracecabiles@gmail.com>
2025-03-03 22:24:36 +0800

    matrix act

--- main.py
+++ main.py
@@ -101,8 +101,108 @@ Thus, the shape of b is (1, 4, 2).'''
 
 # Replace rows 1 and 3 with [[9, 9], [8, 8]]
 # 9,9,8,8 have a shape of 1,2,2 
-b[:, [1, 3], :] = [[[9, 9], [8, 8]]]
-print(b)
-print("Shape of b:", b.shape)
+# b[:, [1, 3], :] = [[[9, 9], [8, 8]]]
+# print(b)
+# print("Shape of b:", b.shape)
 
 # Initialize different types of arrays
+
+# All zero's matrix (5 in a row)
+# print(np.zeros(5))
+# print()
+
+# 2 by 3 matrix of zero's
+# print(np.zeros((2,3)))
+
+# 3d array that is a 2 by 3 by 3 matrix of zero's
+# print()
+# print(np.zeros((2,3,3)))
+
+# 4d array
+# print()
+# print(np.zeros((2,3,3,2)))
+
+# All one's matrix
+# print(np.ones((4,2,2)))
+
+# Custom matrix .full(shape, element)
+# 2 by 2 matrix filled with 99
+# print(np.full((2,2), 99))
+
+# Add data type
+# test = np.full((2,2), 99, dtype='float32')
+# print(test)
+
+# Any other number (full_like)
+# reuse previous array or create an array filled with a specific value
+# full like inherits the data type of a while full with a.shape uses the same shape
+# print(np.full_like(a, 4))
+# print()
+# print(np.full(a.shape, 4))
+
+# Initialize random matrix of numbers
+# print(np.random.rand(4,2))
+
+# Random integer values
+# randint(start value, size) 
+# print(np.random.randint(7))
+# print(np.random.randint(7, size=(3,3)))
+
+# Create a 3d array with 3 columns and rows, 4 and 7 is the random int range
+# print(np.random.randint(4,7, size=(3,3)))
+
+# print(np.random.randint(-4,8, size=(3,3)))
+
+# Identity Matrix is a square matrix, this is a 5 by 5 matrix of 1 and 0
+# print(np.identity(5))
+
+# Repeat an array a few times
+# arr = np.array([1, 2, 3])
+# repeats the numbers inside the array 3 times
+# r1 = np.repeat(arr, 3)
+# print(r1)
+
+# Repeat a 2d array a few times
+# arr = np.array([[1, 2, 3]])
+# axis 0 puts it in a matrix while axis 1 makes it one straight line like before
+# r1 = np.repeat(arr, 3, axis=0)
+# print(r1)
+
+# matrix activity
+# 1 | 1 | 1 | 1 | 1
+# 1 | 0 | 0 | 0 | 1
+# 1 | 0 | 9 | 0 | 1
+# 1 | 0 | 0 | 0 | 1
+# 1 | 1 | 1 | 1 | 1
+
+# code for the matrix activity
+# try code
+# five = np.ones((5,5))
+# five[1][1] = 0
+# five[1][2] = 0
+# five[1][3] = 0
+# five[2][1] = 0
+# five[2][3] = 0
+# five[3][1] = 0
+# five[3][2] = 0
+# five[3][3] = 0
+# five[2][2] = 9
+# print(five)
+
+# Right Code for the activity
+# create a 5 by 5 matrix of ones
+output = np.ones((5,5))
+# create a 3x3 matrix of zeros
+z = np.zeros((3,3))
+# set middle to 9
+z[1,1] = 9
+# move matrix of zeros inside matrix of ones
+# 1 is the row 4 is the column so start at index 1 then end at index 4 (inclusive) or index 3
+# 5x5 matrix is indexed 0 - 4 (inclusive)
+# Inclusive: The starting index is included in the slice.
+# Exclusive: The ending index is not included in the slice.
+# so 1 is the start and inclusive while 4 is the end and exclusive so its actually index 3
+output[1:4, 1:4] = z
+print(output)
+
+# https://youtu.be/QUT1VHiLmmI?si=TFrOJfZKo7N8Mc5e&t=2006
